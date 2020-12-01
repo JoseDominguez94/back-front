6f8551731f1ec945765d94fb241eb427f76a8601
DESKTOP-I06KB15\Jdom0 <josee94@hotmail.es>
2020-12-01 10:20:42 +0100

    plantilla node ts

---
+++ src/database.ts
@@ -0,0 +1,13 @@
+import * as data from './config/config.json';
+import * as Sequelize from 'sequelize';
+
+const db = data.development.database;
+const username = data.development.username;
+const password = data.development.password;
+
+export const database = new Sequelize.Sequelize(db, username , password,{
+    dialect: "mysql",
+    port: 3306,
+
+    
+});
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+app.set('port',3000);
+app.listen(app.get('port'), () =>{
+    console.log(`Server is runing`);
+    
+});
